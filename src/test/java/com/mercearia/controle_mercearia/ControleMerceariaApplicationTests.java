e35c5641e2fd3bbf011b51181978ab3fdf428cac
CMSM1972 <59923370+CMSM1972@users.noreply.github.com>
2025-02-08 16:19:12 -0300

    Initial commit

---
+++ src/test/java/com/mercearia/controle_mercearia/ControleMerceariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercearia.controle_mercearia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleMerceariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
